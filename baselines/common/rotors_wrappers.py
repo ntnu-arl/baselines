d5b1d74e0b77790fa1cdb7235d1d21c892424462
Jan <jan_aleksander@hotmail.com>
2021-04-19 23:44:03 +0200

    nothing works

--- baselines/common/rotors_wrappers.py
+++ baselines/common/rotors_wrappers.py
@@ -147,11 +147,6 @@ class RotorsWrappers:
         # get new obs
         new_obs = self.get_new_obs()
 
-        print("::::::::::::::")
-        print(self.lidar_data.number_of_features)
-        print("Obs:")
-        print(new_obs[0:6])
-        print("::::::::::::::")
         # calculate reward
         action = np.array([command.thrust.x, command.thrust.y, command.thrust.z])
         Qx = self.Q_state.dot(new_obs[0:6])
